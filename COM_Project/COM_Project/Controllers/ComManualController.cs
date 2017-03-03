0dc1909e42f1d58f64ab7a14d3c75db766b183a2
Chris Macco <chrismacco8309@gmail.com>
2017-03-03 12:18:17 -0500

    Adding project files

---
+++ COM_Project/COM_Project/Controllers/ComManualController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace COM_Project.Controllers
+{
+    public class ComManualController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Main");
+        }
+    }
+}
---
+++ COM_Project/COM_Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(COM_Project.Startup))]
+namespace COM_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
